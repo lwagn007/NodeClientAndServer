b83807eef96b0f96fe88d69b6cfca9a0e31ff9ca
Lawrence Wagner <lawrencewagner77@gmail.con>
2019-08-09 18:31:47 -0400

    update

--- server/middleware/userservice.js
+++ server/middleware/userservice.js
@@ -14,29 +14,32 @@ class UserService {
         let username = request.user.username;
         let password = request.user.password;
 
-        if(User.findOne({where: {username: request.user.username }})){
-            return "400";
-        }
-
-        User.create({
-            username: username,
-            passwordHash: bcrypt.hashSync(password, 10)
-        }).then(
-            function createSuccess(user) {
-                var token = jwt.sign({ id: user.id },
-                    process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 });
-                user = {
-                    user: user,
-                    message: "created",
-                    sessionToken: token
-                };
-                //console.log(user);
-                return JSON.stringify(user);
-            },
-            function createError(error) {
-                return error.message;
+        User.findOne({ where: { username: request.user.username } }).then(count => {
+            if (count !== 0) {
+                return "400";
+            } else {
+                User.create({
+                    username: username,
+                    passwordHash: bcrypt.hashSync(password, 10)
+                }).then(
+                    function createSuccess(user) {
+                        var token = jwt.sign({ id: user.id },
+                            process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 });
+                        user = {
+                            user: user,
+                            message: "created",
+                            sessionToken: token
+                        };
+                        //console.log(user);
+                        return JSON.stringify(user);
+                    },
+                    function createError(error) {
+                        return error.message;
+                    }
+                );
+                return user;
             }
-        );
+        });
     }
 }
 
